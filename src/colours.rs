084014ae839262ea738fbfc28c51b164fa464356
William Roe <git@wjlr.org.uk>
2019-04-13 01:56:12 +0100

    Solarized colors

--- src/colours.rs
+++ src/colours.rs
@@ -1,3 +1,75 @@
+use lazy_static::*;
+use std::collections::HashMap;
+
+pub type ColorArray = [f32; 4];
+
+#[derive(Copy, Clone, PartialEq, Eq, Hash)]
+pub enum SolarizedColor {
+    Base03,
+    Base02,
+    Base01,
+    Base00,
+    Base0,
+    Base1,
+    Base2,
+    Base3,
+    Yellow,
+    Orange,
+    Red,
+    Magenta,
+    Violet,
+    Blue,
+    Cyan,
+    Green,
+}
+
+lazy_static! {
+    pub static ref SOLARIZED_COLOR_MAP: HashMap<SolarizedColor, ColorArray> = {
+        let triples = [
+            (SolarizedColor::Base03, (0, 43, 54)),
+            (SolarizedColor::Base02, (7, 54, 66)),
+            (SolarizedColor::Base01, (88, 110, 117)),
+            (SolarizedColor::Base00, (101, 123, 131)),
+            (SolarizedColor::Base0, (131, 148, 150)),
+            (SolarizedColor::Base1, (147, 161, 161)),
+            (SolarizedColor::Base2, (238, 232, 213)),
+            (SolarizedColor::Base3, (253, 246, 227)),
+            (SolarizedColor::Yellow, (181, 137, 0)),
+            (SolarizedColor::Orange, (203, 75, 22)),
+            (SolarizedColor::Red, (220, 50, 47)),
+            (SolarizedColor::Magenta, (211, 54, 130)),
+            (SolarizedColor::Violet, (108, 113, 196)),
+            (SolarizedColor::Blue, (38, 139, 210)),
+            (SolarizedColor::Cyan, (42, 161, 152)),
+            (SolarizedColor::Green, (133, 153, 0)),
+        ];
+
+        let mut m = HashMap::new();
+        for (color_name, color_triple) in triples.iter() {
+            let color_array = [
+                color_triple.0 as f32 / 255.0,
+                color_triple.1 as f32 / 255.0,
+                color_triple.2 as f32 / 255.0,
+                1.0,
+            ];
+            m.insert(*color_name, color_array);
+        }
+        m
+    };
+}
+
+// type ColorTriple = (u8, u8, u8);
+
+// We can't do float math in const fns!!!
+// const fn triple_to_array(triple: ColorTriple) -> ColorArray {
+//     [
+//         triple.0 as f32 / 255.0,
+//         triple.1 as f32 / 255.0,
+//         triple.2 as f32 / 255.0,
+//         1.0,
+//     ]
+// }
+
 pub const BG_COLOR: [f32; 4] = [0.22, 0.55, 0.3, 1.0];
 
 pub const CORRECT_WORD_COLOR: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
