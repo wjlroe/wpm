0c35a6a91eb8e97e85a22e78399cfaa8f9baa033
William Roe <git@wjlr.org.uk>
2019-04-28 01:40:58 +0100

    Extract label out of ResultsScreen

---
+++ src/label.rs
@@ -0,0 +1,53 @@
+use crate::*;
+use cgmath::*;
+use gfx_glyph::{FontId, GlyphCruncher, Scale, Section};
+
+#[derive(Default)]
+pub struct Label {
+    pub font_size: f32,
+    pub font_id: FontId,
+    pub color: ColorArray,
+    pub rect: Rect,
+    pub text: String,
+}
+
+impl Label {
+    pub fn new(
+        font_size: f32,
+        font_id: FontId,
+        color: ColorArray,
+        text: String,
+        gfx_window: &mut GfxWindow,
+    ) -> Self {
+        let mut label = Self {
+            font_size,
+            font_id,
+            color,
+            text,
+            ..Label::default()
+        };
+        label.recalc(gfx_window);
+        label
+    }
+
+    pub fn section(&self, gfx_window: &mut GfxWindow) -> Section {
+        Section {
+            font_id: self.font_id,
+            color: self.color,
+            scale: Scale::uniform(self.font_size * gfx_window.dpi as f32),
+            text: &self.text,
+            ..Section::default()
+        }
+    }
+
+    pub fn recalc(&mut self, gfx_window: &mut GfxWindow) {
+        let section = self.section(gfx_window);
+        if let Some(dim) = gfx_window.glyph_brush.pixel_bounds(section).map(|bounds| {
+            let width = bounds.max.x;
+            let height = bounds.max.y;
+            vec2(width as f32, height as f32)
+        }) {
+            self.rect.bounds = dim;
+        }
+    }
+}
--- src/screens/results_screen.rs
+++ src/screens/results_screen.rs
@@ -2,7 +2,6 @@ use crate::layout::ElementLayout;
 use crate::screens;
 use crate::*;
 use cgmath::*;
-use gfx_glyph::{FontId, GlyphCruncher, Scale, Section};
 use lazy_static::*;
 use std::error::Error;
 
@@ -18,56 +17,6 @@ const HEADLINE_LABEL_FONT_SIZE: f32 = 48.0;
 const HEADLINE_VALUE_FONT_SIZE: f32 = 48.0;
 
 static mut HAVE_PRINTED_SECTIONS: bool = false;
-
-#[derive(Default)]
-struct Label {
-    font_size: f32,
-    font_id: FontId,
-    color: ColorArray,
-    rect: Rect,
-    text: String,
-}
-
-impl Label {
-    fn new(
-        font_size: f32,
-        font_id: FontId,
-        color: ColorArray,
-        text: String,
-        gfx_window: &mut GfxWindow,
-    ) -> Self {
-        let mut label = Self {
-            font_size,
-            font_id,
-            color,
-            text,
-            ..Label::default()
-        };
-        label.recalc(gfx_window);
-        label
-    }
-
-    fn section(&self, gfx_window: &mut GfxWindow) -> Section {
-        Section {
-            font_id: self.font_id,
-            color: self.color,
-            scale: Scale::uniform(self.font_size * gfx_window.dpi as f32),
-            text: &self.text,
-            ..Section::default()
-        }
-    }
-
-    fn recalc(&mut self, gfx_window: &mut GfxWindow) {
-        let section = self.section(gfx_window);
-        if let Some(dim) = gfx_window.glyph_brush.pixel_bounds(section).map(|bounds| {
-            let width = bounds.max.x;
-            let height = bounds.max.y;
-            vec2(width as f32, height as f32)
-        }) {
-            self.rect.bounds = dim;
-        }
-    }
-}
 
 #[derive(Default)]
 pub struct ResultsScreen {
