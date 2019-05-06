a001707fce15013d50616754b54cb8475bba30d8
William Roe <git@wjlr.org.uk>
2019-05-06 18:59:01 +0100

    Remove unnuecessary colors

--- src/colours.rs
+++ src/colours.rs
@@ -87,6 +87,3 @@ lazy_static! {
 }
 
 pub const TRANSPARENT: [f32; 4] = [0.0, 0.0, 0.0, 0.0];
-pub const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-pub const BLUE: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
-pub const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
